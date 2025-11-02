2e2bd5bbb86a6f77dfbf9e42e87e4ee8f96cb8e4
Juliusz Pawełka <juliuszpawelka@MacBook-Pro-Juliusz.local>
2025-11-02 11:31:55 +0100

    feat: start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import GroundTypeSelect from './components/grounditype';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-title-section">
+          <h1 className="justify-center">Kalkulator klinu odłamu</h1>
+          <h2>Aplikacja dla operatorów maszyn budowlanych</h2>
+        </div>
+        <p className="ground-type-label">1. Rodzaj gruntu</p>
+       <div className="ground-type-select-section bg-orange-500 rounded-b-2xl shadow-2xl p-6 md:p-8 align-column">
+            <GroundTypeSelect />
+
+       </div>
+       <p className="ground-type-label">2. Wysokość klinu</p>
+        
+        
+     
       </header>
     </div>
   );
